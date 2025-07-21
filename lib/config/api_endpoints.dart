f26ffa36289eeaed6ce5ca270bd6ce45aed747e1
Deepak Sapkota <iamdpk.spk@gmail.com>
2025-07-21 21:29:50 +0545

    feat: Add API endpoints and popular cities data

---
+++ lib/config/api_endpoints.dart
@@ -0,0 +1,12 @@
+class ApiEndpoints {
+  static const String baseurl =
+      'https://api.openweathermap.org/data/2.5/weather';
+
+  static const String geocodingUrl =
+      'https://api.openweathermap.org/geo/1.0/direct';
+
+  static const String reverseGeocodingUrl =
+      'https://api.openweathermap.org/geo/1.0/reverse';
+
+  static const String iconUrl = 'https://openweathermap.org/img/wn';
+}
